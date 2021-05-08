d1abd831888b8dd139b9e11b4e69ee0c8314c718
masato932 <masatowada66@gmail.com>
2021-05-08 21:59:26 +0900

    commit202105082159

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import View
+from django.shortcuts import render
+from .models import Post
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.order_by("-id")
+        return render(request, 'app/index.html', {
+            'post_data': post_data,
+        })
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'app/post_detail.html', {
+            'post_data': post_data
+        })
