38324190378dca6a95171b7779bca548e495c6be
masato932 <masatowada66@gmail.com>
2021-05-09 12:09:17 +0900

    commit202105091209

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'), # 追加
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'), # 追加
+    path('post/<int:pk>/edit/', views.PostEditView.as_view(), name='post_edit'), # 追加
 ]
